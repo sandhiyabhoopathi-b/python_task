031946eda480ec659c8ccdccb96f1fd71cc25844
sandhiyabhoopathi-b <152408226+sandhiyabhoopathi-b@users.noreply.github.com>
2023-11-29 22:16:03 +0530

    Add files via upload

---
+++ Task/task2.py
@@ -0,0 +1,25 @@
+number1=10
+number2=2
+name="sandhiya"
+num=10
+print("-----------------------")
+print("Variable Example")
+print("-----------------------")
+print(number1 + number2)
+print(number1 - number2)
+print(number1**number2)
+print(number1*number2)
+print(number1/number2)
+print("------------------------")
+print("Find type function")
+print("------------------------")
+print(type(number2))
+print(type(name))
+print("---------")
+print("Id() eg")
+print("---------")
+print(id(number1))
+print(id(number2))
+print(id(name))
+print(id(num))
+

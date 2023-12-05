def4257ae4cae09134b872dd9e09141736d1e9c6
sandhiyabhoopathi-b <152408226+sandhiyabhoopathi-b@users.noreply.github.com>
2023-12-05 06:03:05 +0530

    Add files via upload

---
+++ Task/task3.py
@@ -0,0 +1,12 @@
+Name = input("Enter the name : ")
+Add = input("Enter the address : ")
+Degree = input("Enter Current degree : ")
+s1 = int(input("Enter s1 mark  : "))
+s2 = int(input("Enter s2 mark : "))
+s3 = int(input("Enter s3 mark : "))
+s4 = int(input("Enter s4 mark : "))
+s5 = int(input("Enter s5 mark : "))
+s6 = int(input("Enter s6 mark : "))
+total = s1+s2+s3+s4+s5+s6
+avg = total/6
+print(" I am " , Name , " coming from " , Add , " and i am pursuing  " , Degree , " then  my HSC six subject marks are , the  subject 1 :  " , s1 , " subject 2 : " , s2 , " subject 3 : " , s3 , "subject 4 : " , s4 ,  "subject 5 : ",s5 , "subject 6 : " ,s6 , " and my total is : " , total , "and average is :  " , avg)
